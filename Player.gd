245eedc00386b355c3452483d8ccc67a37fd7ec0
MatiFreyre <mfreyre@gmail.com>
2019-09-29 11:28:37 -0300

    Elimino variable innecesaria, corrijo comentarios, cambio transición

--- Player.gd
+++ Player.gd
@@ -7,12 +7,13 @@ var is_grabbed = false
 var coordinates = Vector2(0,0)
 
 func _ready():
-	# Todavía no sé para qué está esto
-#	set_process(true)
+	# Todavía no sé para qué es esto, viene de ejemplos online pero funciona sin esta línea 
+	# set_process(true)
 	pass
 
 func _process(delta):
-	# Seguir el movimiento del mouse mientras se esté arrastrando
+	# Seguir el movimiento del mouse durante el drag
+	# Ajuste de posición: posición final es superior-izquierda, pero en el drag está en el centro
 	if is_grabbed:
 		self.set_global_position(get_global_mouse_position() + POSITION_ADJUSTMENT)
 
@@ -41,14 +42,13 @@ func snap_position():
 	coordinates = position / CELL_SIZE
 	
 # Mover en exactamente una celda en la dirección correspondiente.
-# TODO Debe haber una mejor forma de acotar el espacio y hacer la transación
+# TODO Debe haber una mejor forma de acotar el espacio y hacer la transición
 func move(direction):
-	var new_coordinates = coordinates - direction
-	var new_position = new_coordinates * CELL_SIZE
+	coordinates -= direction
+	var new_position = coordinates * CELL_SIZE
 	new_position.x = clamp(new_position.x, 0, get_viewport_rect().size.x + CELL_SIZE.x)
 	new_position.y = clamp(new_position.y, 0, get_viewport_rect().size.y + CELL_SIZE.y)
 	# Uso Tween para que la transición no sea instantánea sino contínua
 	$Tween.interpolate_property(self, "position", position, new_position, 
 			0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
-	coordinates = new_coordinates
