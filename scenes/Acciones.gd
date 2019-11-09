cfc59436ff5a0270b9678b7d6aaf7fe95215bf15
gdecuzzi <giseladecuzzi@gmail.com>
2019-11-09 16:30:03 -0300

    agrego script

---
+++ scenes/Acciones.gd
@@ -0,0 +1,9 @@
+extends MarginContainer
+
+# Si no nos toca accionar se grisan las acciones
+var active = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
